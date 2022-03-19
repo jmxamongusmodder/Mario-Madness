82f15465e66665b5223bd29ba3e01b0480372821
jmxamongusmodder <80003646+jmxamongusmodder@users.noreply.github.com>
2022-03-19 19:16:30 -0400

    skippable videos (not part 2 tho!)

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -26,52 +26,48 @@ class FlxVideo extends FlxBasic {
 		{
                     onVLCSkipped();
 		}
-
-		#if web
-		var player:Video = new Video();
-		player.x = 0;
-		player.y = 0;
-		FlxG.addChildBelowMouse(player);
-		var netConnect = new NetConnection();
-		netConnect.connect(null);
-		var netStream = new NetStream(netConnect);
-		netStream.client = {
-			onMetaData: function() {
-				player.attachNetStream(netStream);
-				player.width = FlxG.width;
-				player.height = FlxG.height;
-			}
-		};
-		netConnect.addEventListener(NetStatusEvent.NET_STATUS, function(event:NetStatusEvent) {
-			if(event.info.code == "NetStream.Play.Complete") {
-				netStream.dispose();
-				if(FlxG.game.contains(player)) FlxG.game.removeChild(player);
-
-				if(finishCallback != null) finishCallback();
-			}
-		});
-		netStream.play(name);
-
-		#elseif desktop
-		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
-
-		vlcBitmap = new VlcBitmap();
-		vlcBitmap.set_height(FlxG.stage.stageHeight);
-		vlcBitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
-
-		vlcBitmap.onComplete = onVLCComplete;
-		vlcBitmap.onError = onVLCError;
-
-		FlxG.stage.addEventListener(Event.ENTER_FRAME, fixVolume);
-		vlcBitmap.repeat = 0;
-		vlcBitmap.inWindow = false;
-		vlcBitmap.fullscreen = false;
-		fixVolume(null);
-
-		FlxG.addChildBelowMouse(vlcBitmap);
-		vlcBitmap.play(checkFile(name));
-		#end
 	}
+
+	var player:Video = new Video();
+	player.x = 0;
+	player.y = 0;
+	FlxG.addChildBelowMouse(player);
+	var netConnect = new NetConnection();
+	netConnect.connect(null);
+	var netStream = new NetStream(netConnect);
+	netStream.client = {
+		onMetaData: function() {
+			player.attachNetStream(netStream);
+			player.width = FlxG.width;
+			player.height = FlxG.height;
+		}
+	};
+	netConnect.addEventListener(NetStatusEvent.NET_STATUS, function(event:NetStatusEvent) {
+		if(event.info.code == "NetStream.Play.Complete") {
+			netStream.dispose();
+			if(FlxG.game.contains(player)) FlxG.game.removeChild(player);
+
+			if(finishCallback != null) finishCallback();
+		}
+	});
+	netStream.play(name);
+
+	#if desktop
+	// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
+
+	vlcBitmap = new VlcBitmap();
+	vlcBitmap.set_height(FlxG.stage.stageHeight);
+	vlcBitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
+	vlcBitmap.onComplete = onVLCComplete;
+	vlcBitmap.onError = onVLCError;
+	FlxG.stage.addEventListener(Event.ENTER_FRAME, fixVolume);
+	vlcBitmap.repeat = 0;
+	vlcBitmap.inWindow = false;
+	vlcBitmap.fullscreen = false;
+	fixVolume(null);
+	FlxG.addChildBelowMouse(vlcBitmap);
+	vlcBitmap.play(checkFile(name));
+        #end
 
 	#if desktop
 	function checkFile(fileName:String):String
@@ -128,15 +124,26 @@ class FlxVideo extends FlxBasic {
 
 	public function onVLCSkipped()
 	{
+                // Stop the bitmaps.
 		vlcBitmap.stop();
 
 		// Clean player, just in case!
 		vlcBitmap.dispose();
 
+                // Psycially remove the video.
+//		netStream.dispose(); // HOW DOES IT NOT RECONIZE WHAT THIS IS???
+		if(FlxG.game.contains(player)) FlxG.game.removeChild(player);
+
+                // Finish callbacking.
+		finishCallback();
+               
+                // Mutes the audio
+		vlcBitmap.volume = 0;
+
+                // Checks if the game contains a vlc bitmap.
 		if (FlxG.game.contains(vlcBitmap))
 		{
 			FlxG.game.removeChild(vlcBitmap);
-			finishCallback();
 		}
 	}
 
@@ -148,6 +155,5 @@ class FlxVideo extends FlxBasic {
 				finishCallback();
 			}
 		}
-	#end
 	#end
 }
